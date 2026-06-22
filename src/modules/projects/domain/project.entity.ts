a6880a5ea84ad40f3c11accf39caf73eb1da1ec1
TzzJokerzzT <alexjesus-4856@hotmail.com>
2026-06-22 05:13:37 -0500

    chore(domain): delete postion and change description type

--- src/modules/projects/domain/project.entity.ts
+++ src/modules/projects/domain/project.entity.ts
@@ -1,14 +1,2 @@
 export interface IProject {
-  _id?: string;
-  title: string;
-  position: string;
-  description: string[];
-  imageUrl: string;
-  techStack: string[];
-  repoUrl: string;
-  liveUrl: string;
-  startDate?: Date;
-  endDate?: Date;
-  createdAt?: string;
-  updatedAt?: string;
 }
